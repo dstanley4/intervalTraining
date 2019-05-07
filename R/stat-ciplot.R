a73ff0361eba66a93c8e2aa1a34ba61bd7fd0649
David Stanley <dstanley@uoguelph.ca>
2019-05-07 11:19:11 -0400

    update code order for stat_ciplot and stat_piplot

--- R/stat-ciplot.R
+++ R/stat-ciplot.R
@@ -1,4 +1,54 @@
-#remember to account for missing data
+#' This is the documentation for stat_ciplot (confidence interval plot)
+#' @param mapping Set of aesthetic mappings created by aes() or aes_(). If specified and inherit.aes = TRUE (the default), it is combined with the default mapping at the top level of the plot. You must supply mapping if there is no plot mapping
+#' @param data The data to be displayed in this layer
+#' @param position Position adjustment, either as a string, or the result of a call to a position adjustment function.
+#' @param na.rm If FALSE, the default, missing values are removed with a warning. If TRUE, missing values are silently removed
+#' @param show.legend logical. Should this layer be included in the legends? NA, the default, includes if any aesthetics are mapped. FALSE never includes, and TRUE always includes. It can also be a named logical vector to finely select the aesthetics to display
+#' @param inherit.aes If FALSE, overrides the default aesthetics, rather than combining with them. This is most useful for helper functions that define both data and aesthetics and shouldn't inherit behaviour from the default plot specification, e.g. borders().
+#' @param pop.r Population correlation
+#' @param n Sample size
+#' @param ci.LL Confidence interval lower limit
+#' @param ci.UL Confidence interval upper limit
+#' @param ... Other arguments
+#' @export
+stat_ciplot <- function(mapping = NULL, data = NULL,
+                        position = "identity", na.rm = FALSE, show.legend = NA,
+                        inherit.aes = TRUE, pop.r, n, ci.LL, ci.UL, ...) {
+
+  list(
+    ggplot2::layer(
+      stat = StatSamplingDistributionLL, data = data, mapping = mapping, geom = "path",
+      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+      params = list(na.rm = na.rm, ...)
+    ),
+    ggplot2::layer(
+      stat = StatSamplingDistributionUL, data = data, mapping = mapping, geom = "path",
+      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+      params = list(na.rm = na.rm, ...)
+    ),
+    ggplot2::layer(
+      stat = StatIntervalCI, data = data, mapping = mapping, geom = "errorbarh",
+      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+      params = list(na.rm = na.rm, ...)
+    ),
+    ggplot2::layer(
+      stat = StatDistCenterUL, data = data, mapping = mapping, geom = "path",
+      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+      params = list(na.rm = na.rm, ...)
+    ),
+    ggplot2::layer(
+      stat = StatDistCenterLL, data = data, mapping = mapping, geom = "path",
+      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+      params = list(na.rm = na.rm, ...)
+    ),
+    ggplot2::layer(
+      stat = StatIntervalCenter, data = data, mapping = mapping, geom = "point",
+      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+      params = list(na.rm = na.rm, ...)
+    )
+  )
+}
+
 
 StatSamplingDistributionLL <- ggplot2::ggproto("StatSamplingDistributionOutline", ggplot2::Stat,
                               required_aes = c("x", "y"),
@@ -41,59 +91,7 @@ StatDistCenterLL<- ggplot2::ggproto("StatDistCenterLL", ggplot2::Stat,
 
 )
 
-#remove fill and colour from above functions?
 
-
-#' This is the documentation for stat_ciplot (confidence interval plot)
-#' @param mapping Set of aesthetic mappings created by aes() or aes_(). If specified and inherit.aes = TRUE (the default), it is combined with the default mapping at the top level of the plot. You must supply mapping if there is no plot mapping
-#' @param data The data to be displayed in this layer
-#' @param position Position adjustment, either as a string, or the result of a call to a position adjustment function.
-#' @param na.rm If FALSE, the default, missing values are removed with a warning. If TRUE, missing values are silently removed
-#' @param show.legend logical. Should this layer be included in the legends? NA, the default, includes if any aesthetics are mapped. FALSE never includes, and TRUE always includes. It can also be a named logical vector to finely select the aesthetics to display
-#' @param inherit.aes If FALSE, overrides the default aesthetics, rather than combining with them. This is most useful for helper functions that define both data and aesthetics and shouldn't inherit behaviour from the default plot specification, e.g. borders().
-#' @param pop.r Population correlation
-#' @param n Sample size
-#' @param ci.LL Confidence interval lower limit
-#' @param ci.UL Confidence interval upper limit
-#' @param ... Other arguments
-#' @export
-stat_ciplot <- function(mapping = NULL, data = NULL,
-                         position = "identity", na.rm = FALSE, show.legend = NA,
-                         inherit.aes = TRUE, pop.r, n, ci.LL, ci.UL, ...) {
-
-  list(
-    ggplot2::layer(
-      stat = StatSamplingDistributionLL, data = data, mapping = mapping, geom = "path",
-      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-      params = list(na.rm = na.rm, ...)
-    ),
-    ggplot2::layer(
-      stat = StatSamplingDistributionUL, data = data, mapping = mapping, geom = "path",
-      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-      params = list(na.rm = na.rm, ...)
-    ),
-    ggplot2::layer(
-      stat = StatIntervalCI, data = data, mapping = mapping, geom = "errorbarh",
-      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-      params = list(na.rm = na.rm, ...)
-    ),
-    ggplot2::layer(
-      stat = StatDistCenterUL, data = data, mapping = mapping, geom = "path",
-      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-      params = list(na.rm = na.rm, ...)
-    ),
-    ggplot2::layer(
-      stat = StatDistCenterLL, data = data, mapping = mapping, geom = "path",
-      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-      params = list(na.rm = na.rm, ...)
-    ),
-    ggplot2::layer(
-      stat = StatIntervalCenter, data = data, mapping = mapping, geom = "point",
-      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-      params = list(na.rm = na.rm, ...)
-    )
-  )
-}
 
 
 #####
--- R/stat-piplot.R
+++ R/stat-piplot.R
@@ -1,4 +1,45 @@
-#remember to account for missing data
+#' This is the documentation for stat_piplot (prediction interval plot)
+#' @param mapping Set of aesthetic mappings created by aes() or aes_(). If specified and inherit.aes = TRUE (the default), it is combined with the default mapping at the top level of the plot. You must supply mapping if there is no plot mapping
+#' @param data The data to be displayed in this layer
+#' @param position Position adjustment, either as a string, or the result of a call to a position adjustment function.
+#' @param na.rm If FALSE, the default, missing values are removed with a warning. If TRUE, missing values are silently removed
+#' @param show.legend logical. Should this layer be included in the legends? NA, the default, includes if any aesthetics are mapped. FALSE never includes, and TRUE always includes. It can also be a named logical vector to finely select the aesthetics to display
+#' @param inherit.aes If FALSE, overrides the default aesthetics, rather than combining with them. This is most useful for helper functions that define both data and aesthetics and shouldn't inherit behaviour from the default plot specification, e.g. borders().
+#' @param pop.r Population correlation
+#' @param n Sample size
+#' @param pi.LL Prediction interval lower limit
+#' @param pi.UL Prediction interval upper limit
+#' @param ... Other arguments
+#' @export
+stat_piplot <- function(mapping = NULL, data = NULL,
+                        position = "identity", na.rm = FALSE, show.legend = NA,
+                        inherit.aes = TRUE, pop.r, n, pi.LL, pi.UL, ...) {
+
+  list(
+    ggplot2::layer(
+      stat = StatSamplingDistributionOutline, data = data, mapping = mapping, geom = "path",
+      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+      params = list(na.rm = na.rm, ...)
+    ),
+    ggplot2::layer(
+      stat = StatSamplingDistributionCapture, data = data, mapping = mapping, geom = "polygon",
+      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+      params = list(na.rm = na.rm, ...)
+    ),
+    ggplot2::layer(
+      stat = StatIntervalPI, data = data, mapping = mapping, geom = "errorbarh",
+      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+      params = list(na.rm = na.rm, ...)
+    ),
+    ggplot2::layer(
+      stat = StatIntervalCenter, data = data, mapping = mapping, geom = "point",
+      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+      params = list(na.rm = na.rm, ...)
+    )
+  )
+}
+
+
 
 StatSamplingDistributionOutline <- ggplot2::ggproto("StatSamplingDistributionOutline", ggplot2::Stat,
                               required_aes = c("x", "y"),
@@ -34,46 +75,6 @@ StatIntervalCenter<- ggplot2::ggproto("StatSamplingDistributionCapture", ggplot2
 )
 
 
-#' This is the documentation for stat_piplot (prediction interval plot)
-#' @param mapping Set of aesthetic mappings created by aes() or aes_(). If specified and inherit.aes = TRUE (the default), it is combined with the default mapping at the top level of the plot. You must supply mapping if there is no plot mapping
-#' @param data The data to be displayed in this layer
-#' @param position Position adjustment, either as a string, or the result of a call to a position adjustment function.
-#' @param na.rm If FALSE, the default, missing values are removed with a warning. If TRUE, missing values are silently removed
-#' @param show.legend logical. Should this layer be included in the legends? NA, the default, includes if any aesthetics are mapped. FALSE never includes, and TRUE always includes. It can also be a named logical vector to finely select the aesthetics to display
-#' @param inherit.aes If FALSE, overrides the default aesthetics, rather than combining with them. This is most useful for helper functions that define both data and aesthetics and shouldn't inherit behaviour from the default plot specification, e.g. borders().
-#' @param pop.r Population correlation
-#' @param n Sample size
-#' @param pi.LL Prediction interval lower limit
-#' @param pi.UL Prediction interval upper limit
-#' @param ... Other arguments
-#' @export
-stat_piplot <- function(mapping = NULL, data = NULL,
-                         position = "identity", na.rm = FALSE, show.legend = NA,
-                         inherit.aes = TRUE, pop.r, n, pi.LL, pi.UL, ...) {
-
-  list(
-    ggplot2::layer(
-      stat = StatSamplingDistributionOutline, data = data, mapping = mapping, geom = "path",
-      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-      params = list(na.rm = na.rm, ...)
-    ),
-    ggplot2::layer(
-      stat = StatSamplingDistributionCapture, data = data, mapping = mapping, geom = "polygon",
-      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-      params = list(na.rm = na.rm, ...)
-    ),
-    ggplot2::layer(
-      stat = StatIntervalPI, data = data, mapping = mapping, geom = "errorbarh",
-      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-      params = list(na.rm = na.rm, ...)
-    ),
-    ggplot2::layer(
-      stat = StatIntervalCenter, data = data, mapping = mapping, geom = "point",
-      position = position, show.legend = show.legend, inherit.aes = inherit.aes,
-      params = list(na.rm = na.rm, ...)
-    )
-  )
-}
 
 
 r_to_z <- function(r) {
